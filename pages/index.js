da3b5b3b7c4a78698c254c28b5b6faa2295c4034
AMA Mazing <alexmalone489@gmail.com>
2022-09-28 13:34:37 +1000

    Favicon and Title

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,11 @@ import Counter from "../components/counter"
 export default function Home() {
   return (
     <div>
+      <Head>
+      <title>Counter</title>
+      <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <h1></h1>
       <Counter />
     </div>
