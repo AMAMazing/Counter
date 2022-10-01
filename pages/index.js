d9ce98018f26304949196d518ec6b3fa38920c6a
AMA Mazing <alexmalone489@gmail.com>
2022-10-01 19:43:33 +1000

    Max Scale

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     <div>
       <Head>
         <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
         
         <title>Counter</title>
         <link rel="icon" href="Logo.png" />
